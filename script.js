fc631a1adb7cc820e7d74301b6a7743847b5ee4d
Vigneshvasanth <vigneshvasanth.c@trustrace.com>
2020-06-20 23:07:41 +0530

    css and js file is separatly included

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log(window.location.href);
+console.log(window.location);
+console.log(window);
+console.log(document);
+document.getElementById('content').innerHTML = "<h1>This is my first Java Script code</h1>"
